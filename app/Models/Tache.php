de00e2d2b6bb671e0229556e055992775dc3f29c
merouane amqor <marouane.amqor6@gmail.com>
2021-03-28 13:22:42 +0000

    update

---
+++ app/Models/Tache.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tache extends Model
+{
+    use HasFactory;
+
+    protected $table = 'taches';
+
+    public $primaryKey = 'ma_cle';
+    
+    public $timestamps = false;
+
+}
